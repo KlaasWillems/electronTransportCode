f10538d9181fc85d96d8cefbe92f0e84dcaeb4fb
Klaas Willems <klaas.willems@student.kuleuven.be>
2023-02-16 11:39:14 +0100

    rename variable

--- electronTransportCode/SimOptions.py
+++ electronTransportCode/SimOptions.py
@@ -6,9 +6,9 @@ from electronTransportCode.utils import tuple2d
 class SimOptions(ABC):
     """Object which encapsulates all simulation options for Monte Carlo simulation setings. E.g. number of particles, initial conditions, ....
     """
-    def __init__(self, nbParticles: int, thresholdEnergy: float, rngSeed: int = 12) -> None:
+    def __init__(self, nbParticles: int, minEnergy: float, rngSeed: int = 12) -> None:
         self.nbParticles: int = nbParticles
-        self.thresholdEnergy: float = thresholdEnergy
+        self.minEnergy: float = minEnergy
         self.rng: np.random.Generator = np.random.default_rng(rngSeed)
         
     @abstractmethod
