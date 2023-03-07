bab1b2614eae345411baeac390a9cdd93c79825e
Klaas Willems <klaaswillems1@gmail.com>
2023-03-07 11:23:47 +0100

    waterphantom run

--- numericalExperiments/waterPhantom/startSim.sh
+++ numericalExperiments/waterPhantom/startSim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Run command and leave
 # Source: https://askubuntu.com/questions/1143698/how-to-run-ssh-on-remote-machine-and-leave
-rm data/logfile.txt
-/bin/python3 /home/r0738465/Documents/electronTransportCode/numericalExperiments/waterPhantom/WaterPhantomScript.py >> data/logfile.txt &
+# rm data/logfile.txt
+/bin/python3 -O /home/r0738465/Documents/electronTransportCode/numericalExperiments/waterPhantom/WaterPhantomScript.py >> data/logfile.txt &
 disown
 # tail -f data/logfile.txt
