61590f3e8241aa35944d9c7ddcd12f4926a2d07e
Klaas Willems <klaas.willems@student.kuleuven.be>
2023-03-25 18:27:45 +0100

    MPI advectionDiffusionlimit

--- numericalExperiments/diffusionLimit/AdvectionDiffusionlimit/AdvectionDiffusionlimitScript.py
+++ numericalExperiments/diffusionLimit/AdvectionDiffusionlimit/AdvectionDiffusionlimitScript.py
@@ -16,10 +16,10 @@ from electronTransportCode.ParticleModel import DiffusionTestParticle
 from electronTransportCode.SimOptions import DiffusionPointSource
 from electronTransportCode.MCParticleTracer import AnalogParticleTracer
 
-# Initialize Advection-Diffusion parameters (No advection)
-StoppingPower = 1
-scatteringRate1 = 1
-scatteringRate2 = 100
+# Initialize Advection-Diffusion parameters
+stoppingPower = 1
+scatteringRate1 = '(1 + 0.5*sin(x))'; scatteringRate1_dx = '(-0.5*cos(x)/((1 + 0.5*sin(x))**2))'
+scatteringRate2 = '(100 + 10*sin(x))'; scatteringRate2_dx = '(cos(x)/(10*(10 + sin(x))**2))'
 varH = 1/3  # 1 in 1D, 1/3 in 3D
 sigmaInit = 1.0
 muInit = 0.0
@@ -30,8 +30,9 @@ xbins = 512
 xmax = 15.0
 
 # initialize the equation and the space
-eq1 = PDE({"φ": f"laplace({varH}*φ/({StoppingPower}*{scatteringRate1}))"})
-eq2 = PDE({"φ": f"laplace({varH}*φ/({StoppingPower}*{scatteringRate2}))"})
+eq1 = PDE({"φ": f"laplace({varH}*φ/({stoppingPower}*{scatteringRate1})) - d_dx(φ*{varH}*{scatteringRate1_dx}/{stoppingPower})"})
+eq2 = PDE({"φ": f"laplace({varH}*φ/({stoppingPower}*{scatteringRate2})) - d_dx(φ*{varH}*{scatteringRate2_dx}/{stoppingPower})"})
+
 grid = CartesianGrid([(-xmax, xmax)], [xbins], periodic=True)
 state = ScalarField.from_expression(grid, f"exp(-0.5*((x-{muInit})/{sigmaInit})**2)/({sigmaInit}*sqrt(2*{np.pi}))")
 
@@ -43,10 +44,13 @@ zmin = -xmax; zmax = xmax; zbins = 2
 simDomain = SimulationDomain(ymin, ymax, zmin, zmax, ybins, zbins, material=unitDensityMaterial)
 
 # Set up initial conditions
-
+NB_PARTICLES = 30000
 eSource: float = tmax
 SEED: int = 4  # Random number generator seed
 pointSourceSim = DiffusionPointSource(minEnergy=0.0, rngSeed=SEED, eSource=eSource, loc=muInit, std=sigmaInit)
+
+# Scattering angle must be independent of previous angle!
+DiffusionPointSource.SAMPLE_NEW_ABSOLUTE_DIRECTION = True  # type: ignore
 
 particle1 = DiffusionTestParticle(Es=scatteringRate1)
 particle2 = DiffusionTestParticle(Es=scatteringRate2)
@@ -66,7 +70,6 @@ if __name__ == '__main__':
     # - Set up estimator and particle
     trackEndEstimatorx1 = TrackEndEstimator(simDomain, NB_PARTICLES_PER_PROC, setting='x')
     trackEndEstimatorx2 = TrackEndEstimator(simDomain, NB_PARTICLES_PER_PROC, setting='x')
-
 
     # - Run simulation
     particleTracer1.runMultiProc(nbParticles=NB_PARTICLES, estimators=(trackEndEstimatorx1, ), file=file1)
