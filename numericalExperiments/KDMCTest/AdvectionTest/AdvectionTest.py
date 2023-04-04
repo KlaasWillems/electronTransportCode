3df987186442d22584a8d9a604523eb0d9340952
Klaas Willems <klaas.willems@student.kuleuven.be>
2023-04-04 11:32:47 +0200

    Same test with smaller stepsize

--- numericalExperiments/KDMCTest/AdvectionTest/AdvectionTest.py
+++ numericalExperiments/KDMCTest/AdvectionTest/AdvectionTest.py
@@ -32,7 +32,8 @@ if __name__ == '__main__':
 
     pointSourceSim = KDTestSource(minEnergy=0.0, rngSeed=SEED, eSource=eSource)
     particleTracerK = AnalogParticleTracer(particle=None, simOptions=pointSourceSim, simDomain=simDomain)
-    particleTracerKD = KDParticleTracer(particle=None, simOptions=pointSourceSim, simDomain=simDomain, dS = eSource)  # stepsize is final time!
+    particleTracerKD1 = KDParticleTracer(particle=None, simOptions=pointSourceSim, simDomain=simDomain, dS = eSource)  # stepsize is final time!
+    particleTracerKD2 = KDParticleTracer(particle=None, simOptions=pointSourceSim, simDomain=simDomain, dS = eSource/2)
 
     NB_PARTICLES = int(sys.argv[2])
     NB_PARTICLES_PER_PROC = int(NB_PARTICLES/nproc)
@@ -45,6 +46,9 @@ if __name__ == '__main__':
     TrackEndEstimatorKD1 = TrackEndEstimator(simDomain, nb_particles=NB_PARTICLES_PER_PROC, setting='x')
     TrackEndEstimatorKD2 = TrackEndEstimator(simDomain, nb_particles=NB_PARTICLES_PER_PROC, setting='x')
     TrackEndEstimatorKD3 = TrackEndEstimator(simDomain, nb_particles=NB_PARTICLES_PER_PROC, setting='x')
+    TrackEndEstimatorKD4 = TrackEndEstimator(simDomain, nb_particles=NB_PARTICLES_PER_PROC, setting='x')
+    TrackEndEstimatorKD5 = TrackEndEstimator(simDomain, nb_particles=NB_PARTICLES_PER_PROC, setting='x')
+    TrackEndEstimatorKD6 = TrackEndEstimator(simDomain, nb_particles=NB_PARTICLES_PER_PROC, setting='x')
 
     t1 = time.perf_counter()
 
@@ -55,9 +59,12 @@ if __name__ == '__main__':
     particleTracerK.runMultiProc(nbParticles=NB_PARTICLES, estimators=(TrackEndEstimatorK3, ), particle=particle3, file='data/TrackEndEstimatorK3.pkl', logAmount=logAmount)
 
     # Run KD particle tracer
-    particleTracerKD.runMultiProc(nbParticles=NB_PARTICLES, estimators=(TrackEndEstimatorKD1, ), particle=particle1, file='data/TrackEndEstimatorKD1.pkl', logAmount=logAmount)
-    particleTracerKD.runMultiProc(nbParticles=NB_PARTICLES, estimators=(TrackEndEstimatorKD2, ), particle=particle2, file='data/TrackEndEstimatorKD2.pkl', logAmount=logAmount)
-    particleTracerKD.runMultiProc(nbParticles=NB_PARTICLES, estimators=(TrackEndEstimatorKD3, ), particle=particle3, file='data/TrackEndEstimatorKD3.pkl', logAmount=logAmount)
+    particleTracerKD1.runMultiProc(nbParticles=NB_PARTICLES, estimators=(TrackEndEstimatorKD1, ), particle=particle1, file='data/TrackEndEstimatorKD1.pkl', logAmount=logAmount)
+    particleTracerKD1.runMultiProc(nbParticles=NB_PARTICLES, estimators=(TrackEndEstimatorKD2, ), particle=particle2, file='data/TrackEndEstimatorKD2.pkl', logAmount=logAmount)
+    particleTracerKD1.runMultiProc(nbParticles=NB_PARTICLES, estimators=(TrackEndEstimatorKD3, ), particle=particle3, file='data/TrackEndEstimatorKD3.pkl', logAmount=logAmount)
+    particleTracerKD2.runMultiProc(nbParticles=NB_PARTICLES, estimators=(TrackEndEstimatorKD4, ), particle=particle1, file='data/TrackEndEstimatorKD4.pkl', logAmount=logAmount)
+    particleTracerKD2.runMultiProc(nbParticles=NB_PARTICLES, estimators=(TrackEndEstimatorKD5, ), particle=particle2, file='data/TrackEndEstimatorKD5.pkl', logAmount=logAmount)
+    particleTracerKD2.runMultiProc(nbParticles=NB_PARTICLES, estimators=(TrackEndEstimatorKD6, ), particle=particle3, file='data/TrackEndEstimatorKD6.pkl', logAmount=logAmount)
 
     t2 = time.perf_counter()
 
@@ -72,6 +79,7 @@ if __name__ == '__main__':
 
         # dump particle tracers
         pickle.dump(particleTracerK, open('data/particleTracerK.pkl', 'wb'))
-        pickle.dump(particleTracerKD, open('data/particleTracerKD.pkl', 'wb'))
+        pickle.dump(particleTracerKD1, open('data/particleTracerKD1.pkl', 'wb'))
+        pickle.dump(particleTracerKD2, open('data/particleTracerKD2.pkl', 'wb'))
 
     # run: mpiexec -n 4 python3 -m simpleHomogeneous 1.0 30000
