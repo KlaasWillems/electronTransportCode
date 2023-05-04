d1fb1229a59b336ef8db85b4ec737a7d6cd5c760
Klaas Willems <klaas.willems@student.kuleuven.be>
2023-05-04 21:28:06 +0200

    Better load balancing

--- ms/ownMSv2.py
+++ ms/ownMSv2.py
@@ -1,7 +1,9 @@
 import sys
+import os
 import math
-from typing import Optional
+import pickle
 import time
+from typing import Optional
 import numpy as np
 from mpi4py import MPI
 from electronTransportCode.SimOptions import KDRLUTSimulation
@@ -15,48 +17,31 @@ from electronTransportCode.Material import Material
 xmin = -100
 xmax = 100
 xbins = ybins = 1
+tempFile = 'data/tempTEE.pkl'
 
-def sample(nbsamples: int, energy: float, stepsize: float, material: Material) -> tuple[Optional[float], Optional[float], Optional[float], Optional[float], Optional[float], Optional[float], Optional[float]]:
+def sample(NB_PARTICLES: int, NB_PARTICLES_PER_PROC: int, energy: float, stepsize: float, material: Material) -> None:
     simDomain = SimulationDomain(xmin, xmax, xmin, xmax, xbins, ybins, material)
     particle = KDRTestParticle(KDR=False)
     deltaE = particle.energyLoss(energy, np.array((0, 0, 0), dtype=float), stepsize, material)
-    if energy > deltaE:
-        # assert energy > deltaE, f'{energy=}, {deltaE=}, {stepsize=}. Stepsize too large for available energy'
-        simOptions = KDRLUTSimulation(eSource=energy, minEnergy=energy-deltaE, rngSeed=MPI.COMM_WORLD.Get_rank())
-        TEEx = TrackEndEstimator(simDomain, nbsamples, 'x')
-        TEEy = TrackEndEstimator(simDomain, nbsamples, 'y')
-        TEEz = TrackEndEstimator(simDomain, nbsamples, 'z')
-        particleTracer = AnalogParticleTracer(simOptions, simDomain, particle)
-        particleTracer.__call__(nbsamples, (TEEx, TEEy, TEEz))
 
-        temp = np.zeros_like(TEEx.scoreMatrix)  # cos(theta) array
-        for i in range(nbsamples):
-            x = TEEx.scoreMatrix[i]
-            y = TEEy.scoreMatrix[i]
-            z = TEEz.scoreMatrix[i]
-            temp[i] = z/math.sqrt(x**2 + y**2 + z**2)
-
-        return temp.mean(), TEEx.scoreMatrix.mean(), TEEy.scoreMatrix.mean(), TEEz.scoreMatrix.mean(), TEEx.scoreMatrix.var(), TEEy.scoreMatrix.var(), TEEz.scoreMatrix.var()
-    else:
-        print(f'{energy=}, {deltaE=}, {stepsize=}')
-        return None, None, None, None, None, None, None
+    simOptions = KDRLUTSimulation(eSource=energy, minEnergy=energy-deltaE, rngSeed=MPI.COMM_WORLD.Get_rank())
+    TEEx = TrackEndEstimator(simDomain, NB_PARTICLES_PER_PROC, 'x')
+    TEEy = TrackEndEstimator(simDomain, NB_PARTICLES_PER_PROC, 'y')
+    TEEz = TrackEndEstimator(simDomain, NB_PARTICLES_PER_PROC, 'z')
+    particleTracer = AnalogParticleTracer(simOptions, simDomain, particle)
+    particleTracer.runMultiProc(NB_PARTICLES, (TEEx, TEEy, TEEz), file=tempFile, verbose=False)
 
 if __name__ == '__main__':
-    nbsamples = int(float(sys.argv[1]))
-    nproc = MPI.COMM_WORLD.Get_size()
     rank = MPI.COMM_WORLD.Get_rank()
-    nbds = int(float(sys.argv[2]))
-
-    if nbds < nproc:
-        raise ValueError('More processors than stepsize values.')
+    nproc = MPI.COMM_WORLD.Get_size()
+    NB_PARTICLES_PER_PROC = int(float(sys.argv[1])/nproc)
+    NB_PARTICLES = int(NB_PARTICLES_PER_PROC*nproc)
 
     # stepsize array
     minds = 1e-4
     maxds = 0.1
     nbds = int(float(sys.argv[2]))
-    stepsizes = np.linspace(minds, maxds, nbds)
-    stepsizesSplit = np.array_split(stepsizes, nproc)
-    stepsizeArray = stepsizesSplit[rank]
+    stepsizeArray = np.linspace(minds, maxds, nbds)
 
     # material array. Specific for lung test case.
     minDensity = 0.05
@@ -64,7 +49,11 @@ if __name__ == '__main__':
     nbDensity = int(float(sys.argv[3]))
     densityArray = np.linspace(minDensity, maxDensity, nbDensity)
 
-    lut = np.empty(shape=(stepsizeArray.size, nbDensity, 7), dtype=float)
+    lut: Optional[np.ndarray]
+    if rank == 0:
+        lut = np.empty(shape=(stepsizeArray.size, nbDensity, 7), dtype=float)
+    else:
+        lut = None
 
     t1 = time.process_time()
 
@@ -73,31 +62,25 @@ if __name__ == '__main__':
         t3 = time.process_time()
         for k, density in enumerate(densityArray):
             material = Material(rho=density)
-            lut[j, k, :] = sample(nbsamples, EnergyDummy, stepsize, material)
+            sample(NB_PARTICLES, NB_PARTICLES_PER_PROC, EnergyDummy, stepsize, material)
+            if rank == 0:
+                assert lut is not None
+                TEEx, TEEy, TEEz = pickle.load(open(tempFile, 'rb'))
+                temp = np.zeros_like(TEEx.scoreMatrix)  # cos(theta) array
+                for i in range(NB_PARTICLES):
+                    x = TEEx.scoreMatrix[i]
+                    y = TEEy.scoreMatrix[i]
+                    z = TEEz.scoreMatrix[i]
+                    temp[i] = z/math.sqrt(x**2 + y**2 + z**2)
+                lut[j, k, :] = temp.mean(), TEEx.scoreMatrix.mean(), TEEy.scoreMatrix.mean(), TEEz.scoreMatrix.mean(), TEEx.scoreMatrix.var(), TEEy.scoreMatrix.var(), TEEz.scoreMatrix.var()
         t4 = time.process_time()
-        print(f'Proc: {rank}. {round(100*(j+1)/stepsizeArray.size, 3)}% completed. Last section took {(t4-t3)/60:2e} minutes.')
-
-    bigLut: Optional[np.ndarray]
-    if nproc > 1:
         if rank == 0:
-            bigLut = np.empty(shape=(nbds, nbDensity, 7), dtype=float)
-            beginIndex = 0
-            for index, energyGroup in enumerate(stepsizesSplit):
-                if index != 0:
-                    MPI.COMM_WORLD.Recv(bigLut[beginIndex:, :, :], source=index, tag=index)
-                else:
-                    bigLut[beginIndex:beginIndex+stepsizeArray.size, :, :] = lut
-                beginIndex += stepsizeArray.size
-
-        else:
-            MPI.COMM_WORLD.Send(lut, dest=0, tag=rank)
-            bigLut = None
-    else:
-        bigLut = lut
+            print(f'{round(100*(j+1)/stepsizeArray.size, 3)}% completed. Last section took {(t4-t3)/60:2e} minutes.')
 
     if rank == 0:
-        assert bigLut is not None
-        np.save('data/ownlutv2.npy', bigLut)
+        assert lut is not None
+        os.remove(tempFile)
+        np.save('data/ownlutv2.npy', lut)
         np.savez('data/ownlutAxesv2.npz', stepsizeArray, densityArray)
         t2 = time.process_time()
         print(f'Total time: {(t2-t1)/60:2e} minutes.')
