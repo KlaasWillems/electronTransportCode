138f3bd31be571115a42800a3f172cee7ca5471d
Klaas Willems <klaas.willems@student.kuleuven.be>
2023-05-17 14:27:32 +0200

    Updated moment QOI

--- numericalExperiments/KDMCTest/momentumQoi/momentumQoi.py
+++ numericalExperiments/KDMCTest/momentumQoi/momentumQoi.py
@@ -2,17 +2,18 @@ import sys
 import pickle
 import time
 from mpi4py import MPI
+from electronTransportCode import SimOptions
 from electronTransportCode.SimulationDomain import SimulationDomain
 from electronTransportCode.Material import unitDensityMaterial
 from electronTransportCode.SimOptions import PointSource
 from electronTransportCode.ParticleModel import DiffusionTestParticle, DiffusionTestParticlev2
 from electronTransportCode.MCParticleTracer import AnalogParticleTracer, KDMC
-from electronTransportCode.MCEstimator import MomentumTypeEstimator
+from electronTransportCode.MCEstimator import DoseEstimator, MomentumTypeEstimator
 
 
 # Set up simulation domain
 xmax = 10
-ymin = -xmax; ymax = xmax; ybins = 200  # No internal grid cell crossings
+ymin = -xmax; ymax = xmax; ybins = 200
 zmin = -xmax; zmax = xmax; zbins = 200
 simDomain = SimulationDomain(ymin, ymax, zmin, zmax, ybins, zbins, material=unitDensityMaterial)
 
@@ -30,13 +31,13 @@ if __name__ == '__main__':
 
     if particleType == 1:
         particle = DiffusionTestParticle(Es=scatteringRate, sp=1.0)
-        fileK = 'data/TrackEndEstimatorK1.pkl'
-        fileKD = 'data/TrackEndEstimatorKD1.pkl'
+        fileK = 'data/EstimatorsK1.pkl'
+        fileKD = 'data/EstimatorsKD1.pkl'
         simargvFile = 'data/simargv1.pkl'
     elif particleType == 2:
         particle = DiffusionTestParticlev2(Es=scatteringRate, sp=1.0)
-        fileK = 'data/TrackEndEstimatorK2.pkl'
-        fileKD = 'data/TrackEndEstimatorKD2.pkl'
+        fileK = 'data/EstimatorsK2.pkl'
+        fileKD = 'data/EstimatorsKD2.pkl'
         simargvFile = 'data/simargv2.pkl'
     else:
         raise ValueError('Invalid particle type')
@@ -49,19 +50,17 @@ if __name__ == '__main__':
     NB_PARTICLES_PER_PROC = int(NB_PARTICLES/nproc)
     NB_PARTICLES = NB_PARTICLES_PER_PROC*nproc
 
-    momEstimatorK1 = MomentumTypeEstimator(simDomain=simDomain)
-    momEstimatorKD1 = MomentumTypeEstimator(simDomain=simDomain)
-    momEstimatorK2 = MomentumTypeEstimator(simDomain=simDomain)
-    momEstimatorKD2 = MomentumTypeEstimator(simDomain=simDomain)
+    momEstimatorK = MomentumTypeEstimator(simDomain=simDomain); doseEstimatorK = DoseEstimator(simDomain=simDomain)
+    momEstimatorKD = MomentumTypeEstimator(simDomain=simDomain); doseEstimatorKD = DoseEstimator(simDomain=simDomain)
 
     t1 = time.perf_counter()
 
     # Run analog particle tracer
     logAmount = int(NB_PARTICLES_PER_PROC/10)
-    particleTracerK.runMultiProc(nbParticles=NB_PARTICLES, estimators=(momEstimatorK1, ), file=fileK, logAmount=logAmount)
+    particleTracerK.runMultiProc(nbParticles=NB_PARTICLES, estimators=(momEstimatorK, doseEstimatorK), file=fileK, logAmount=logAmount)
 
     # Run KD particle tracer
-    particleTracerKD.runMultiProc(nbParticles=NB_PARTICLES, estimators=(momEstimatorKD1, ), file=fileKD, logAmount=logAmount)
+    particleTracerKD.runMultiProc(nbParticles=NB_PARTICLES, estimators=(momEstimatorKD, doseEstimatorKD), file=fileKD, logAmount=logAmount)
 
     t2 = time.perf_counter()
 
