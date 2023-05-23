e2f33c71bba2a4864a10fa900b1bf5414ff4df85
Klaas Willems <klaas.willems@student.kuleuven.be>
2023-05-23 21:49:56 +0200

    Lung test update

--- numericalExperiments/KuschLungTest/lungTest.py
+++ numericalExperiments/KuschLungTest/lungTest.py
@@ -4,7 +4,7 @@ import time
 import numpy as np
 from mpi4py import MPI
 from electronTransportCode.MCEstimator import DoseEstimator
-from electronTransportCode.ParticleModel import SimplifiedEGSnrcElectron
+from electronTransportCode.ParticleModel import SimplifiedEGSnrcElectron, KDRTestParticle, ParticleModel
 from electronTransportCode.MCParticleTracer import AnalogParticleTracer, KDMC, KDR
 from electronTransportCode.ProjectUtils import ERE
 from lungSetup import LungInitialConditions, LungSimulationDomain
@@ -15,9 +15,16 @@ if __name__ == '__main__':
     NB_PARTICLES = int(sys.argv[1])
     scatterer = sys.argv[2]
     algorithm = sys.argv[3]
+    particleType = sys.argv[4]
 
     # Sim settings
-    particle = SimplifiedEGSnrcElectron(scatterer=scatterer)  # constrain scattering to yz plance
+    particle: ParticleModel
+    if particleType == 'EGS':
+        particle = SimplifiedEGSnrcElectron(scatterer=scatterer)  # constrain scattering to yz plance
+    elif particleType == 'KDRTest':
+        particle = KDRTestParticle(msDist='vmf')
+    else:
+        raise ValueError
     lungInit = LungInitialConditions(sigmaPos=1/50, kappa=10, eSource=35/ERE)
     lungSimDomain = LungSimulationDomain()
 
@@ -31,6 +38,7 @@ if __name__ == '__main__':
 
     NB_PARTICLES_PER_PROC = int(NB_PARTICLES/procs)
     NB_PARTICLES = int(procs*NB_PARTICLES_PER_PROC)
+    _, step = np.linspace(0, lungSimDomain.width, lungSimDomain.bins+1, retstep=True)
 
     if algorithm == 'k':
         # Load particle tracers
@@ -44,7 +52,6 @@ if __name__ == '__main__':
         pickle.dump(analogTracer, open(f'data/analogTracer{scatterer}.pkl', mode='wb'))
     elif algorithm == 'kd':
         # Load particle tracers
-        _, step = np.linspace(0, lungSimDomain.width, lungSimDomain.bins+1, retstep=True)
         kdmc = KDMC(lungInit, lungSimDomain, particle, dS=step)  # type: ignore
 
         # Estimator
@@ -54,8 +61,7 @@ if __name__ == '__main__':
         t2 = time.process_time()
         pickle.dump(kdmc, open(f'data/kdmc{scatterer}.pkl', mode='wb'))
     elif algorithm == 'kdr':
-        _, step = np.linspace(0, lungSimDomain.width, lungSimDomain.bins+1, retstep=True)
-        kdr = KDR(simOptions=lungInit, simDomain=lungSimDomain, particle=particle, dS=step)  # type: ignore
+        kdr = KDR(simOptions=lungInit, simDomain=lungSimDomain, particle=particle, dS=step, useMSAngle=False)  # type: ignore
 
         # Estimator
         doseEstimatorKD = DoseEstimator(lungSimDomain)
@@ -63,6 +69,16 @@ if __name__ == '__main__':
         kdr.runMultiProc(NB_PARTICLES, (doseEstimatorKD, ), file=f'data/doseEstimatorKDR{scatterer}.pkl', logAmount=500)
         t2 = time.process_time()
         pickle.dump(kdr, open(f'data/kdr{scatterer}.pkl', mode='wb'))
+    elif algorithm == 'kdrMS':
+        kdr = KDR(simOptions=lungInit, simDomain=lungSimDomain, particle=particle, dS=step, useMSAngle=True)  # type: ignore
+
+        # Estimator
+        doseEstimatorKD = DoseEstimator(lungSimDomain)
+        t1 = time.process_time()
+        kdr.runMultiProc(NB_PARTICLES, (doseEstimatorKD, ), file=f'data/doseEstimatorKDRMS{scatterer}.pkl', logAmount=500)
+        t2 = time.process_time()
+        pickle.dump(kdr, open(f'data/kdrMS{scatterer}.pkl', mode='wb'))
+
     else:
         raise ValueError('Wrong algorithm input.')
 
