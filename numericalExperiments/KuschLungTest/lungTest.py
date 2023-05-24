34745326c5b6d1392e2dbe0ca0fdad95656d651d
Klaas Willems <klaas.willems@student.kuleuven.be>
2023-05-24 21:38:37 +0200

    Potential bug fix

--- numericalExperiments/KuschLungTest/lungTest.py
+++ numericalExperiments/KuschLungTest/lungTest.py
@@ -49,7 +49,8 @@ if __name__ == '__main__':
         t1 = time.process_time()
         analogTracer.runMultiProc(NB_PARTICLES, (doseEstimatorK, ), file=f'data/doseEstimatorK{scatterer}.pkl', logAmount=500)
         t2 = time.process_time()
-        pickle.dump(analogTracer, open(f'data/analogTracer{scatterer}.pkl', mode='wb'))
+        if rank == 0:
+            pickle.dump(analogTracer, open(f'data/analogTracer{scatterer}.pkl', mode='wb'))
     elif algorithm == 'kd':
         # Load particle tracers
         kdmc = KDMC(lungInit, lungSimDomain, particle, dS=step)  # type: ignore
@@ -59,7 +60,8 @@ if __name__ == '__main__':
         t1 = time.process_time()
         kdmc.runMultiProc(NB_PARTICLES, (doseEstimatorKD, ), file=f'data/doseEstimatorKD{scatterer}.pkl', logAmount=500)
         t2 = time.process_time()
-        pickle.dump(kdmc, open(f'data/kdmc{scatterer}.pkl', mode='wb'))
+        if rank == 0:
+            pickle.dump(kdmc, open(f'data/kdmc{scatterer}.pkl', mode='wb'))
     elif algorithm == 'kdr':
         kdr = KDR(simOptions=lungInit, simDomain=lungSimDomain, particle=particle, dS=step, useMSAngle=False)  # type: ignore
 
@@ -68,7 +70,8 @@ if __name__ == '__main__':
         t1 = time.process_time()
         kdr.runMultiProc(NB_PARTICLES, (doseEstimatorKD, ), file=f'data/doseEstimatorKDR{scatterer}.pkl', logAmount=500)
         t2 = time.process_time()
-        pickle.dump(kdr, open(f'data/kdr{scatterer}.pkl', mode='wb'))
+        if rank == 0:
+            pickle.dump(kdr, open(f'data/kdr{scatterer}.pkl', mode='wb'))
     elif algorithm == 'kdrMS':
         kdr = KDR(simOptions=lungInit, simDomain=lungSimDomain, particle=particle, dS=step, useMSAngle=True)  # type: ignore
 
@@ -77,7 +80,8 @@ if __name__ == '__main__':
         t1 = time.process_time()
         kdr.runMultiProc(NB_PARTICLES, (doseEstimatorKD, ), file=f'data/doseEstimatorKDRMS{scatterer}.pkl', logAmount=500)
         t2 = time.process_time()
-        pickle.dump(kdr, open(f'data/kdrMS{scatterer}.pkl', mode='wb'))
+        if rank == 0:
+            pickle.dump(kdr, open(f'data/kdrMS{scatterer}.pkl', mode='wb'))
 
     else:
         raise ValueError('Wrong algorithm input.')
